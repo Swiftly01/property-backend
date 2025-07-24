6e82287da1b2628897fba32d2668ad7c115952fa
swiftly01 <kareemkazeem100@gmail.com>
2025-07-24 17:23:43 +0100

    fixed activity log

--- resources/lang/en/activity.php
+++ resources/lang/en/activity.php
@@ -4,5 +4,6 @@ return [
   'sell_request_submitted' => 'New sell request submitted by :name',
   'property_listing' => 'New property has been listed',
   'buy_request' => 'New buy request submitted by :name',
+  'all_request' => 'New reqest submitted'
 
 ];
